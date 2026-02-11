959701f028b82ea87c98d9c4aaea2cd7de024ae7
islam <islam.cheche@yandex.ru>
2026-02-11 20:03:28 +0300

    s

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,7 +2,14 @@ import './Menu.css'
 
 function Menu(){
     return(
-        <div className="MenuDiv"></div>
+        <div className="MenuDiv">
+            <div className='icon-menu'></div>
+            <ul className='Menu-list'>
+                <li>ДОПОЛНИТЕЛЬНО</li>
+                <li>3D</li>
+                <li>НАВЫКИ</li>
+            </ul>
+        </div>
     )
 }
 
